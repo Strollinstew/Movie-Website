748719f1d01cb5eb077e5d9905c3f73df856fddd
Strollinstew <kentdeangelo2011@gmail.com>
2026-06-23 17:37:21 +0700

    hmmm

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 include 'koneksi.php';
 $query = "SELECT * FROM movie";
 $result = $koneksi->query($query);
+
+$queryo = "SELECT movie.*, genre.* FROM movie JOIN genre ON movie.idGenre = genre.idGenre;";
+$resulto = $koneksi->query($queryo);
 ?>
 <!doctype html>
 <html lang="en">
@@ -30,21 +33,37 @@ $result = $koneksi->query($query);
             <a href="#">Sign Up</a>
         </div>
     </nav>
-
-    <div class="container-cards-menu">
+    <div class="scope product-cards | -fluid-text">
         <h1 class="title-top-rated">Top rated</h1>
-        <div class="container-cards">
+        <div class="_card-list | grid" style="
+      --grid--fill-mode: var(--product-cards--fill-mode, auto-fill);
+      --grid--row-gap: var(--product-cards--gap, 1.5rem);
+      --grid--column-gap: var(--product-cards--gap, 1.5rem);
+      --grid--column-max-count: var(--product-cards--column-max-count, 4);
+      --grid--column-min-width: var(--product-cards--column-min-width, 20rem);">
+
             <?php while ($row = $result->fetch_assoc()) { ?>
-                <div class="card">
-                    <img src="<?php echo $row['image']; ?>" class="card-img" />
-                    <div class="card-overview">
-                        <h2><?php echo $row['title']; ?></h2>
-                        <p class="card-year"><?php echo $row['year']; ?></p>
-                        <p class="card-genre"><?php echo $row['genre']; ?></p>
-                        <p class="card-rating"><?php echo $row['rating']; ?></p>
-                        <p class="card-description"><?php echo $row['description']; ?></p>
+                <section class="_card">
+                    <h2 class="_heading | -fluid-text -trim-both" style="--fluid-text--min-font-size: 16;
+      --fluid-text--max-font-size: 24;"><?php echo $row['title']; ?></h2>
+                    <div class="_thumbnail-stack">
+                        <img src="<?php echo $row['image']; ?>" alt="" width="400" height="210" fetchpriority="high" />
+                        <img src="<?php echo $row['image']; ?>" alt="" width="400" height="210" fetchpriority="high" />
                     </div>
-                </div>
+                    <p class="_category | -trim-both">Category A</p>
+                    <p class="_price | -trim-both"><?php echo $row['year']; ?></p>
+                    <p class="_description | -line-clamp"><?php echo $row['description']; ?></p>
+                    <p class="_description | -line-clamp"><?php echo $row['rating']; ?></p>
+                    <ul class="_tag-list | cluster" style="
+      --cluster--gap: 0.5rem;">
+      <?php while ($rowy = $resulto->fetch_assoc()) {?>
+                        <li class="_tag | -trim-both"><?php echo $rowy['nameGenre']?></li>
+                        <?php } ?>
+                    <div class="_button"
+                        style="--purchase-button--background: var(--_accent); --purchase-button--foreground: var(--_accent-contrast);">
+                        <a href="#" class="scope purchase-button">See more</a>
+                    </div>
+                </section>
             <?php } ?>
         </div>
     </div>
